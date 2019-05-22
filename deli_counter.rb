a863365a2b3fbef3247811d45d7ee841935b1114
Jiayi Wang <clairejw1015@gmail.com>
2019-05-22 23:16:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    line_order = "The line is currently:"
+    line_order = ["The line is currently:"]
     katz_deli.each do |person|
       line_order.push(" #{i}.#{person}")
       i += 1
